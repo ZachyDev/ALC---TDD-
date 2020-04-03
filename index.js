1597e2c0b998d632ff4e77aaf2f1296fcbe6c3d9
ZachyDev <mosetizachary001@gmail.com>
2020-04-03 17:25:26 +0300

    updated index.test.js

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 // define an object of methods
+// const axios = require('axios');
 const functions = {
     add : (a,b) => a +b,
     product: (a,b) => a * b,
     isNull: () => null,
+    checkValue: x => x,
+    createUser: ()=>{
+        const user = {
+             firstName: 'Zachy',
+             lastName: 'Moseti',
+    }
+    return user;
+    },
+    fetchUser: ()=>{
+        axios.get()
+                .then(res=>res.data)
+                .catch(err=>'error')
+    },
+   reverseString: (str) =>{
+       str
+            .split()
+            .reverse()
+            .join();
+   }
 }
 // export the functions
 module.exports = functions;
--- index.test.js
+++ index.test.js
@@ -1,6 +1,7 @@
 // import the function  methods
 const functions  = require('./index');
 // start a test case for addition
+// toBe
 test('Add 2 + 2 equal to 4',()=>{
     expect(functions.add(10,20)).toBe(30);
 });
@@ -8,7 +9,49 @@ test('Add 2 + 2 equal to 4',()=>{
 test('Multiply 10 * 20 equal to 200',()=>{
     expect(functions.product(10,20)).toBe(200);
 })
-// toBeNull merger
+// toBeNull matcher
 test('Should be null',()=>{
     expect(functions.isNull()).toBeNull();
 })
+// toBeFalsy matcher
+test('Should be falsy',()=>{
+    expect(functions.checkValue(0)).toBeFalsy();
+})
+// toEqual
+test('Should be an object with first and lastname',()=>{
+    expect(functions.createUser()).toEqual({
+        firstName:'Zachy',
+        lastName:'Moseti'
+    });
+});
+// less than and greater than
+test('Should  be less tahn 2000',()=>{
+    // declare two variables
+    let num1 = 1000;
+    let num2 = 800;
+    expect(num1+num2).toBeLessThan(2000);
+});
+// Regex
+test('There should not be w in boy',()=>{
+    expect('boy').not.toMatch(/W/i);
+
+});
+// arrays
+test('admin should be in username',()=>{
+    let usernames = ['moseti','doroh','admin'];
+    expect(usernames).toContain('admin');
+});
+// promise
+// test('Expect name to be Graham ',()=>{
+//     expect.assertions(1);
+//     functions.fetchUser().then(data=>{
+//         expect(data.name),toEqual('Graham');
+//     });
+// });
+// reverse string
+test('reverse string function exists',()=>{
+    expect(functions.reverseString).toBeDefined();
+});
+// test('should reverse',()=>{
+//     expect(functions.reverseString('hello')).toEqual('olleh');
+// })
